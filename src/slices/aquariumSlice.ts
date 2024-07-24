e7cfa03cdb12605a8d52dce193296005641f99ba
Jossl123 <jossl1.gau@gmail.com>
2024-07-24 19:25:27 +0200

    feat : add aquarium state

---
+++ src/slices/aquariumSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { AquariumState } from "../types/state"
+import { AllyInfo, Robot } from "../types/world"
+
+const initialState: AquariumState = {
+  clickedRobot: null,
+};
+
+export const aquariumSlice = createSlice({
+  name: "aquarium",
+  initialState: {} as AquariumState,
+  reducers: {
+    setClickedBot(state, action: PayloadAction<Robot<AllyInfo>>) {
+      return {
+        clickedRobot: action.payload,
+      }
+    },
+  },
+})
+
+export const { setClickedBot } = aquariumSlice.actions
+
+export default aquariumSlice.reducer
